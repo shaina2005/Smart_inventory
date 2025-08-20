dc36de6ab5b4924a4f5eb79b6206b8cd317759b3
shaina2005 <0506shyna@gmail.com>
2025-08-20 20:26:38 +0530

    profile detail container added yet to be designed

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -13,13 +13,41 @@ function Settings() {
               <img src={profile} alt="user name" />
             </div>
             <div className="name-container">
-            <div className="name">PCTE</div>
+              <div className="name">PCTE</div>
             </div>
           </div>
 
           {/* column 2 */}
           <div className="right">
-            Name: PCTE
+            <div className="profile-details">
+              <h3>Profile Details</h3>
+              <div className="details-grid">
+                <div className="detail-item">
+                  <span className="label">Name :</span>
+                  <span className="value">PCTE</span>
+                </div>
+                <div className="detail-item">
+                  <span className="label">Role :</span>
+                  <span className="value">Admin</span>
+                </div>
+                <div className="detail-item">
+                  <span className="label">Email :</span>
+                  <span className="value">pcte123@gmail.com</span>
+                </div>
+                <div className="detail-item">
+                  <span className="label">Phone :</span>
+                  <span className="value">1234567890</span>
+                </div>
+                <div className="detail-item">
+                  <span className="label">Gender :</span>
+                  <span className="value">Male</span>
+                </div>
+                <div className="detail-item">
+                  <span className="label">Location :</span>
+                  <span className="value">India</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
