d7ca35688b09eea75d32c77cdbe2de823c59ffb9
vishesh-s01 <vishesh01327@gmail.com>
2025-09-15 12:15:50 +0530

    sidebar on/off outside working

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,20 +1,35 @@
 import React from "react";
+import { useRef , useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { MdOutlineGridView } from "react-icons/md";
 import { FaChartLine, FaChartBar, FaCog } from "react-icons/fa";
 import { FiMenu } from "react-icons/fi";
 const Sidebar = ({ isOpen, toggleSidebar }) => {
   const location = useLocation();
-
+  const sidebarRef = useRef();
   const menuItems = [
     { path: "/dashboard", label: "Dashboard", icon: <MdOutlineGridView size={20} /> },
     { path: "/inventory", label: "Inventory", icon: <FaChartLine size={20} /> },
     { path: "/reports", label: "Reports & Analysis", icon: <FaChartBar size={20} /> },
     { path: "/settings", label: "Settings", icon: <FaCog size={20} />,extraPath:"/profile"} 
   ];
+   useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        if (isOpen)
+           toggleSidebar();
+      }
+    };
 
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Cleanup
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen, toggleSidebar]);
   return (
-    <div className={`sidebar-modern ${isOpen ? "open" : ""}`}>
+    <div ref={sidebarRef} className={ `sidebar-modern ${isOpen ? "open" : ""}`}>
       <div className="sidebar-logo">
         <div className="hamburger" onClick={toggleSidebar}>
           <FiMenu size={25} />
