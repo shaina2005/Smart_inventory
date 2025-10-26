4bbb8b05877ab47ac09c5855bb5b649e12e39337
simar1305 <simarpreetkaur1305@gmail.com>
2025-10-26 09:02:59 +0530

    logo change

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
-import Logo from "../assets/Logo.png";
+import Logo from "../assets/InventoryLogo.png";
 
 function Login({ onLoginSuccess }) {
   const [role, setRole] = useState("");
