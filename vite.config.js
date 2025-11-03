6488da6d23f4277c71f7f21a006f2087aa79ec00
shaina2005 <0506shyna@gmail.com>
2025-11-03 12:18:01 +0530

    new change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:  '/smart_inventory/',
-  base : '/',
    server: {
     historyApiFallback: true
   }
