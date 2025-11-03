5d946766e02242b5a9d75fd71efca9dfb59db5d9
shaina2005 <0506shyna@gmail.com>
2025-11-03 17:38:02 +0530

    meow

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/smart_inventory">
+    <BrowserRouter basename="/Smart_inventory">
       <App />
     </BrowserRouter>
   </StrictMode>
